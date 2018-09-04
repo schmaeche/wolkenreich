f97a8761a89a8ba2764831746119ee70771c4dd4
Andreas Werner <andi@Finlaggan.local>
2018-09-05 02:18:36 +0900

    switched to layer and source feature of mapbox for displaying states

--- js/mapboxctrl.js
+++ js/mapboxctrl.js
@@ -33,6 +33,70 @@ MBC.initialize = function() {
     fitBoundsOptions: {maxZoom: 9},
     trackUserLocation: false
   }));
+
+  map.on('load', function () {
+    map.addSource("opensky-states-all", {
+      "type": "geojson",
+      "data": {
+        "type": "FeatureCollection",
+        "features": []
+      }
+    });
+
+    map.addLayer({
+      "id": "planes_flying",
+      "type": "symbol",
+      "source": "opensky-states-all",
+      "layout": {
+          "icon-image": "{icon}-15",
+          "icon-rotate": { "type": "identity", "property": "true_track"},
+          "text-field": "{callsign}",
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-offset": [0, 0.8],
+          "text-anchor": "top",
+          "text-size": 10
+      },
+      "paint": {
+        "icon-color": "#b11010",
+        "text-color": "#b11010",
+        "text-halo-color": "#eceeed",
+        "text-halo-width": 1,
+        "text-halo-blur": 0.5
+      }
+    });
+  });
+
+  var popup = new mapboxgl.Popup({
+    closeButton: true,
+    closeOnClick: false
+  });
+
+  map.on('mouseenter', 'planes_flying', function(e) {
+    // Change the cursor style as a UI indicator.
+    map.getCanvas().style.cursor = 'pointer';
+
+    var coordinates = e.features[0].geometry.coordinates.slice();
+    var description = e.features[0].properties.description;
+
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+
+    // Populate the popup and set its coordinates
+    // based on the feature found.
+    popup.setLngLat(coordinates)
+        .setHTML(description)
+        .addTo(map);
+  });
+
+  map.on('mouseleave', 'planes_flying', function() {
+    map.getCanvas().style.cursor = '';
+    //popup.remove();
+  });
+
 }
 
 //###############################
@@ -47,41 +111,50 @@ MBC.getViewLocation = function() {
 MBC.showMarker = function( statesJson) {
   planeMarkerList.clear();
   var states = JSON.parse(statesJson);
+  var data = {
+    "type": "FeatureCollection",
+    "features": []
+  }; //data
+
   for( i in states.states) {
     var state = states.states[i];
-    // create a DOM element for the marker
-    var el = document.createElement('div');
-    el.className = 'marker';
-    el.style.backgroundImage = 'url(res/airplane-icon_big@0,1x.png)';
-    el.style.width = '24px';
-    el.style.height = '24px';
-    el.innerHTML = state[1];
-    var tooltip = "<div class='tooltip'>";
+    var tooltip = "<div class='tooltip_gg'>";
     tooltip += "<div><b>icao: </b>" + state[0] + "</div>";
     tooltip += "<div><b>callsign: </b>" + state[1] + "</div>";
-    tooltip += "<div><b>origin: </b>" + state[2] + "</div>";
-    tooltip += "<div><b>time: </b>" + new Date(state[3] * 1000) + "</div>";
-    tooltip += "<div><b>last contact: </b>" + new Date(state[4] * 1000) + "</div>";
     tooltip += "<div><b>longitude: </b>" + state[5] + "</div>";
     tooltip += "<div><b>latitude: </b>" + state[6] + "</div>";
     tooltip += "<div><b>geo alt: </b>" + state[7] + "m</div>";
     tooltip += "<div><b>baro alt: </b>" + state[13] + "m</div>";
     tooltip += "<div><b>on ground: </b>" + state[8] + "</div>";
     tooltip += "<div><b>velocity: </b>" + state[9] + "m/s</div>";
-    tooltip += "<div><b>track: </b>" + state[10] + "deg</div>";
+    tooltip += "<div><b>heading: </b>" + state[10] + "deg</div>";
     tooltip += "<div><b>vertical: </b>" + state[11] + "m/s</div>";
+    tooltip += "<div><b>origin: </b>" + state[2] + "</div>";
+    tooltip += "<div><b>time: </b>" + new Date(state[3] * 1000) + "</div>";
+    tooltip += "<div><b>last contact: </b>" + new Date(state[4] * 1000) + "</div>";
     tooltip += "<div><b>sensors: </b>" + state[12] + "</div>";
     tooltip += "<div><b>squak: </b>" + state[14] + "</div>";
     tooltip += "<div><b>spi: </b>" + state[15] + "</div>";
     tooltip += "<div><b>pos src: </b>" + state[16] + "</div>";
     tooltip += "</div>";
-    var marker = new mapboxgl.Marker(el)
-      .setLngLat( [parseFloat( state[5]), parseFloat( state[6])])
-      .setPopup( new mapboxgl.Popup({ offset: 25 }) // add popups
-      .setHTML( tooltip))
-      .addTo(map);
-    planeMarkerList.add( marker);
-    //var icon = new PlaneMarkerIcon( {html: title});
-    //layerPlane.addLayer( L.marker([parseFloat(state[6]), parseFloat(state[5])], {icon: icon}).bindTooltip(tooltip, {permanent: false}));
-  }
+
+    data.features.push({
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [ parseFloat(state[5]), parseFloat(state[6]) ]
+      },
+      "properties": {
+        "icon": "airport",
+        "icao": state[0],
+        "callsign": state[1],
+        "longitude": parseFloat(state[5]),
+        "latitude": parseFloat(state[6]),
+        "true_track": state[10],
+        "description": tooltip
+      }
+    });
+
+  } // for
+  map.getSource('opensky-states-all').setData(data);
 }
